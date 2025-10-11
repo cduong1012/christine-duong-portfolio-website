4c91d4e7ed2acea7a0ae2d7a1120e9132755a3ca
Christine Duong <cduong1012@yahoo.com>
2025-10-11 01:29:33 -0700

    udated landing url

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/christine-duong-portfolio-website",
+  base: "/christine-duong-portfolio-website/",
   plugins: [
     react({
       babel: {
