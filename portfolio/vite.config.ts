78f8bbc938896d23dd34bee4801900d31e73915a
chrisld1 <chrisld1@uci.edu>
2025-10-10 23:50:25 -0700

    added host name

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Christine-Duong-New-Portfolio-Website/',
   plugins: [
     react({
       babel: {
