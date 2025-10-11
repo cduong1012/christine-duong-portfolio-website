cf6b075fc07efc027f31ee56cdd37b80c8ecc9d9
Christine Duong <cduong1012@yahoo.com>
2025-10-11 01:33:33 -0700

    updated url

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: "/christine-duong-portfolio-website/",
+export default defineConfig(({ command }) => ({
+  base: command === 'serve' ? '/' : '/christine-duong-portfolio-website/',
   plugins: [
     react({
       babel: {
@@ -11,4 +11,8 @@ export default defineConfig({
       },
     }),
   ],
-})
+}));
+
+
+
+  // base: "/christine-duong-portfolio-website/",
