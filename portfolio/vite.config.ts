0da9b2061c37de0c37efc93f65edf7892c4c21b0
Christine Duong <cduong1012@yahoo.com>
2025-10-11 01:03:34 -0700

    setup github pages

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/christine-duong-portfolio-website",
   plugins: [
     react({
       babel: {
