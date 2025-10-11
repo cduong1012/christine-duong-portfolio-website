7a62e51bf7d6288cd4494c849d843f54400f502a
Christine Duong <cduong1012@yahoo.com>
2025-10-11 00:29:12 -0700

    fixed configurations

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/Christine-Duong-New-Portfolio-Website/',
+export default defineConfig(({ command }) => ({
+  base: command === 'serve' ? '/' : '/Christine-Duong-New-Portfolio-Website/',
   plugins: [
     react({
       babel: {
@@ -11,4 +10,4 @@ export default defineConfig({
       },
     }),
   ],
-})
+}));
